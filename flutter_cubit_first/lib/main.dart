db8dc9db02e43a9defb439c477bb9f61f2fada09
SunilPdl <mrsunilpaudel@gmail.com>
2024-07-24 13:08:03 +0545

    add : flutter cubit file

---
+++ flutter_cubit_first/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Cubit',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '0',
+          style: TextStyle(fontSize: 52.0),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            heroTag: 'Increment',
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.remove),
+            heroTag: 'Decrement',
+          ),
+        ],
+      ),
+    );
+  }
+}
